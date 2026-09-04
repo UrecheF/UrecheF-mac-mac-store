c344cc2ea05a0bcc5b2e4ebad0c39e9080f6fab1
Carlos Ureche <106552972+UrecheF@users.noreply.github.com>
2026-09-04 14:18:22 -0500

    refactor: export stable storefront components

---
+++ web/src/components/storefront/index.js
@@ -0,0 +1,6 @@
+export { default as Newsletter } from './Newsletter';
+export { default as SocialCommunity } from './SocialCommunity';
+export { default as StoreBenefits } from './StoreBenefits';
+export { default as StoreFooter } from './StoreFooter';
+export { default as TechnicalService } from './TechnicalService';
+export { default as Testimonials } from './Testimonials';
