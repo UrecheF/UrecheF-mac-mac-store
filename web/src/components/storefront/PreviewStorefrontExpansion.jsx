7492136a67a43247cdc414735227420b12468378
Carlos Ureche <106552972+UrecheF@users.noreply.github.com>
2026-09-04 14:21:07 -0500

    feat: add isolated storefront expansion preview composition

---
+++ web/src/components/storefront/PreviewStorefrontExpansion.jsx
@@ -0,0 +1,11 @@
+import PromotionalCarousel from './PromotionalCarousel';
+import StorefrontExpansion from './StorefrontExpansion';
+
+export default function PreviewStorefrontExpansion() {
+  return (
+    <main>
+      <PromotionalCarousel />
+      <StorefrontExpansion />
+    </main>
+  );
+}
