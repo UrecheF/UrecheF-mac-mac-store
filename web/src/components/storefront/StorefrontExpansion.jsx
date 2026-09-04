460002ffe39b13c696e242bdcddb37c982e309d9
Carlos Ureche <106552972+UrecheF@users.noreply.github.com>
2026-09-04 14:20:11 -0500

    feat: compose advisor and lower storefront expansion

---
+++ web/src/components/storefront/StorefrontExpansion.jsx
@@ -0,0 +1,12 @@
+import './assistant.css';
+import AssistantTeaser from './AssistantTeaser';
+import LowerStorefront from './LowerStorefront';
+
+export default function StorefrontExpansion() {
+  return (
+    <>
+      <AssistantTeaser />
+      <LowerStorefront />
+    </>
+  );
+}
